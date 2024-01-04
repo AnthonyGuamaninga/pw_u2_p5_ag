098be2289e4767bf887a5da713c0b36e67744a71
Anthony Guamaninga <anthonysub300@gmail.com>
2024-01-03 19:53:20 -0500

    Taller 18: componentes de Vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome Alfred to Your Vue.js App"/>
-  <HelloWorld msg="Hola"/>
-  <HelloWorld msg="Mundo"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Contador titulo="Contador1"/>
+    <Contador titulo="Contador2"/>
+    <Contador titulo="Contador3"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Contador from './components/Contador.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Contador
+    
   }
 }
 </script>
