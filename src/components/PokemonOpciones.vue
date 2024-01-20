5299be32a7d67e3db27c0d69405f4016190f07b8
Anthony Guamaninga <anthonysub300@gmail.com>
2024-01-20 10:59:03 -0500

    Taller25: Comunicación entre componentes

--- src/components/PokemonOpciones.vue
+++ src/components/PokemonOpciones.vue
@@ -1,36 +1,42 @@
 <template>
   <div class="options-container">
     <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
+      <li v-for="pokemon in pokemons" :key="pokemon.id" @click="$emit('selecciono', {id: pokemon.id, nomb: pokemon.nombre} )">
+        {{ pokemon.nombre }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    pokemons: {
+      type: Array,
+      required: true,
+    },
+  },
+};
 </script>
 
 <style>
-ul{
-    list-style-type: none;
+ul {
+  list-style-type: none;
 }
-li{
-    background-color: white;
-    border-radius: 5px;
-    border: 1px solid gray;
-    width: 250px;
-    margin-bottom: 10px;
-    cursor: pointer;
+li {
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid gray;
+  width: 250px;
+  margin-bottom: 10px;
+  cursor: pointer;
 }
-li:hover{
-    background-color: bisque;
+li:hover {
+  background-color: bisque;
 }
-.options-container{
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.options-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,7 +1,16 @@
 <template>
-  <h1>Adivina el pokemon</h1>
-  <PokemonImagen :pokemonId="2" :showPokemon="false" />
-  <PokemonOpciones />
+  <h1 v-if="!pokemonGanador">Espere porfavor, estamos cargando el juego</h1>
+  <div v-else>
+    <h1>Adivina el pokemon</h1>
+    <PokemonImagen
+      :pokemonId="pokemonGanador.id"
+      :showPokemon="mostrarPokemon"
+    />
+    <PokemonOpciones
+      :pokemons="pokemonsArr"
+      @selecciono="revisarClick($event)"
+    />
+  </div>
 </template>
 
 <script>
@@ -15,16 +24,58 @@ export default {
     PokemonImagen,
     PokemonOpciones,
   },
+
+  berofeCreate() {
+    console.log("Antes de crear el componente");
+  },
+  created() {
+    console.log("Se creo el componente");
+  },
+  beforeMount() {
+    console.log("Antes de que se monte el componente en la página");
+  },
   mounted() {
     console.log("Se montó el componente PokemonPage");
     this.cargaInicial();
   },
+  beforeUpdate() {
+    console.log("Antes de que se actulice el componente");
+  },
+  updated() {
+    console.log("Se actualiza el componente");
+  },
+  beforeDestroy() {
+    console.log("Antes de destruir");
+  },
+  destroyed() {
+    console.log("destruido");
+  },
+
   methods: {
     async cargaInicial() {
       const arregloPokemons = await obtenerPokemonsFachada();
       console.log("desde componente");
       console.log(arregloPokemons);
+      this.pokemonsArr = arregloPokemons;
+      const indiceGanador = Math.floor(Math.random() * 4);
+      this.pokemonGanador = this.pokemonsArr[indiceGanador];
     },
+    revisarClick(datoRecibido) {
+      console.log("Dio click y reporto desde el padre");
+      console.log("dato rec: " + datoRecibido.id);
+      console.log("dato rec: " + datoRecibido.nomb);
+      this.mostrarPokemon = true;
+      if (this.pokemonGanador.id === datoRecibido.id) {
+        console.log("Ganó");
+      }
+    },
+  },
+  data() {
+    return {
+      pokemonsArr: [],
+      pokemonGanador: null,
+      mostrarPokemon: false,
+    };
   },
 };
 </script>
